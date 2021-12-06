ce3d6ec84fad40b6cb85f08389b057b4ff603801
clubix <kirillsamarchenko@gmail.com>
2021-12-06 22:44:34 +0300

    add Button

--- myFirstApp/ViewController.swift
+++ myFirstApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
+        helloLabel.textColor = .systemRed
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startPressed() {
+        helloLabel.isHidden.toggle()
+        
+        if helloLabel.isHidden {
+            startButton.setTitle("Show text", for: .normal)
+        } else {
+            startButton.setTitle("Hide text", for: .normal)
+        }
+    }
+    
 }
 
